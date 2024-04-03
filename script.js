2c36e0e30680d4ed2187375e7e99ed077d9379eb
Léo Lourenço <leo.lsrd@gmail.com>
2024-04-02 23:57:24 -0300

    feat: Finalizado a aula sobre arrays e loop

--- script.js
+++ script.js
@@ -1,45 +1,8 @@
-const nav = document.querySelector("nav");
-const produtos = document.querySelector(".produtos a");
+const links = document.querySelectorAll("nav a");
 
-nav.classList.add("add");
-
-console.log(nav);
-console.log(produtos.innerHTML);
-
-// Objetos
-const nome = "O Senhor dos Anéis";
-const ano = 1954;
-const autor = "J. R. R Tolkien";
-
-const nomeFilme = "O Senhor dos Anéis";
-const anoFilme = 2001;
-const diretorFilme = "Peter Jackson";
-
-const livro = {
-  nome: "O Senhor dos Anéis",
-  ano: 1954,
-  autor: "J. R. R. Tolkien",
-};
-
-const filme = {
-  nome: "O Senhor dos Anéis",
-  ano: 2001,
-  autor: "J. R. R. Tolkien",
-  diretor: "Peter Jackson",
-};
-
-console.log(livro);
-
-// Verificando apenas o ano de lançamento do livro
-console.log(livro.ano);
-
-// Verificando a quanto tempo o livro foi lançado
-console.log(
-  "Quantidade de tempo que o livro foi lançado: " + (2024 - livro.ano)
-);
-
-function linguagens(linguagem1, linguagem2, linguagem3 = null) {
-  console.log(linguagem1, linguagem2, linguagem3);
+function logHref(item) {
+  const href = item.href;
+  console.log(href);
 }
 
-linguagens("HTML", "CSS", "JavaScript");
+links.forEach(logHref);
